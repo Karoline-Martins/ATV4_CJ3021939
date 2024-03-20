cf8c509b634ee7b0777db922aa7e23d20d03dbbf
Karoline-Martins <martins.karoline@aluno.ifsp.edu.br>
2024-03-20 08:54:03 -0300

    Adicionar arquivos de projeto.

---
+++ ATV4_CJ3021939/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+using System.ComponentModel;
+
+int n1, n2, op, resultado;
+
+Console.WriteLine("Digite o valor  do primeiro número");
+n1 = int.Parse(Console.ReadLine());
+ Console.WriteLine("Digite o valor do segundo número");
+ n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite 1 para adição");
+Console.WriteLine("Digite 2 para divisão");
+Console.WriteLine("Digite 3 para subtração");
+Console.WriteLine("Digite 4 para multiplicação");
+op = int.Parse(Console.ReadLine());
+
+switch (op)
+{
+    case 1:
+        resultado = n1 +n2;
+
+        Console.WriteLine(resultado);
+        
+        break;
+
+        case 2:
+        resultado = n1 /n2;
+        Console.WriteLine(resultado);
+        
+        break;
+
+        case 3:
+        resultado = n1 - n2;
+        Console.WriteLine(resultado);
+        
+        break;
+
+        case 4:
+        resultado = n1 * n2; 
+        Console.WriteLine(resultado);
+        
+        break;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
